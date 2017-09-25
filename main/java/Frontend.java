088708d25804c2cea7992fc6e3c62e2ed3919026
Morvin <morvin@tut.by>
2017-09-25 14:07:17 +0300

    without import

--- main/java/Frontend.java
+++ main/java/Frontend.java
@@ -4,12 +4,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+package PACKAGE_NAME;
+
 /**
  * Created by Admin on 25.09.2017.
  */
+<<<<<<< 95f01408f04fdbf97efd1c5646dfa51336179122
 <<<<<<< 50b9673fd19f203623036f24026a98c0fd8af236
 public class Frontend extends HttpServlet {
     private String login = "";
+public class Frontend extends HttpServlet {
+    private String login = ' ';
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 
