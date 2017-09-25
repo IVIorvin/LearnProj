69efdee722cb92c82ec082d99473137fee803b1d
Morvin <morvin@tut.by>
2017-09-25 14:37:02 +0300

    make a nice kod ))

--- main/java/Frontend.java
+++ main/java/Frontend.java
@@ -1,18 +1,16 @@
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-package PACKAGE_NAME;
+
 
 /**
  * Created by Admin on 25.09.2017.
  */
-<<<<<<< 95f01408f04fdbf97efd1c5646dfa51336179122
-<<<<<<< 50b9673fd19f203623036f24026a98c0fd8af236
-public class Frontend extends HttpServlet {
-    private String login = "";
+
 public class Frontend extends HttpServlet {
     private String login = "";
 
