ec9292c3bb56e6f40383c042c3cd109b84bd542e
Morvin <morvin@tut.by>
2017-09-25 14:07:17 +0300

    without import

--- main/java/Frontend.java
+++ main/java/Frontend.java
@@ -3,5 +3,14 @@ package PACKAGE_NAME;
 /**
  * Created by Admin on 25.09.2017.
  */
-public class Frontend {
+public class Frontend extends HttpServlet {
+    private String login = ' ';
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+
+    }
 }
